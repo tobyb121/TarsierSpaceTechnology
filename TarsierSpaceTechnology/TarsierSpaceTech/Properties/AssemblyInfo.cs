023bcb2dc55a418195a5f7927cce624593822530
Jamie Leighton <jamieleighton@bigpond.com>
2016-11-04 15:45:14 +1100

    Fix ChemCam crashing the editor scene.

--- TarsierSpaceTechnology/TarsierSpaceTech/Properties/AssemblyInfo.cs
+++ TarsierSpaceTechnology/TarsierSpaceTech/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.6")]
+[assembly: AssemblyVersion("6.6.1")]
 [assembly: KSPAssembly("TarsierSpaceTech", 6, 6)]
 
